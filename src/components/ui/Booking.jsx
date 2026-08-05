5cbfcbaa1420b4a515c1b58ea4ac0b55cbe7b1b8
ben.boehler <ben.boehler@students.fhnw.ch>
2026-08-05 16:02:02 +0200

    add new booking section and related files

---
+++ src/components/ui/Booking.jsx
@@ -0,0 +1,11 @@
+export default function Booking() {
+    return (
+        <section className="booking-section">
+            <iframe
+                src="https://booking.beispiel-webseiten.creaocchio.ch"
+                title="Termin buchen"
+                className="booking-frame"
+            />
+        </section>
+    );
+}
